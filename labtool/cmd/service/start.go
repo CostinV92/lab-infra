c7dfc2e7e7ddf212619912954079d9b18018064a
Victor-Gabriel Costin <costinv92@gmail.com>
2025-07-17 17:25:19 +0300

    labtool: move init of shellCmd in service command
    Signed-off-by: Victor-Gabriel Costin <costinv92@gmail.com>

--- labtool/cmd/service/start.go
+++ labtool/cmd/service/start.go
@@ -4,10 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package service
 
 import (
-	"fmt"
 	"labtool/cmd/configure"
-	"os"
-	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -19,19 +16,8 @@ var startCmd = &cobra.Command{
 	Long:  "start a service",
 	Args:  cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		shellCmd := exec.Command("ansible-playbook")
-
-		shellCmd.Args = append(shellCmd.Args, "-i", configure.Cfg.InventoryPath)
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("target_host=%s", Host))
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("service=%s", Service))
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("admin_user=%s", configure.Cfg.AdminUser))
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("scripts_dir=%s", configure.Cfg.ScriptsDir))
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("services_dir=%s", configure.Cfg.ServicesDir))
-		shellCmd.Args = append(shellCmd.Args, "-e", fmt.Sprintf("env_dir=%s", configure.Cfg.ServicesEnvDir))
+		shellCmd.Args = append(shellCmd.Args, extraVars...)
 		shellCmd.Args = append(shellCmd.Args, configure.Cfg.PlaybookDir+"/start_service.yaml")
-
-		shellCmd.Stdout = os.Stdout
-		shellCmd.Stderr = os.Stderr
 
 		shellCmd.Run()
 	},
